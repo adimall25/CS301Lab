5a06ad44fd71fd064a1d4e3b24b36376069fd9e5
Adarsh Mall <adarshmall1000@gmail.com>
2021-09-30 20:29:56 +0530

    Implement OperandFetch and OF_EX_LatchType

--- as3/CS311_assignment3/src/processor/pipeline/OF_EX_LatchType.java
+++ as3/CS311_assignment3/src/processor/pipeline/OF_EX_LatchType.java
@@ -3,6 +3,10 @@ package processor.pipeline;
 public class OF_EX_LatchType {
 	
 	boolean EX_enable;
+	int branchTarget;
+	int op1;
+	int op2;
+	int imm;
 	
 	public OF_EX_LatchType()
 	{
@@ -15,6 +19,46 @@ public class OF_EX_LatchType {
 
 	public void setEX_enable(boolean eX_enable) {
 		EX_enable = eX_enable;
+	}
+
+	public void setBranchTarget(int target)
+	{
+		branchTarget = target;
+	}
+
+	public void setOp1(int op1)
+	{
+		this.op1 = op1;
+	}
+
+	public void setOp2(int op2)
+	{
+		this.op2 = op2;
+	}
+
+	public void setImm(int imm)
+	{
+		this.imm = imm;
+	}
+
+	public int getBranchTarget()
+	{
+		return branchTarget;
+	}
+
+	public int getOp1()
+	{
+		return this.op1;
+	}
+
+	public int getOp2()
+	{
+		return this.op2;
+	}
+
+	public int getImm()
+	{
+		return this.imm;
 	}
 
 }
--- as3/CS311_assignment3/src/processor/pipeline/OperandFetch.java
+++ as3/CS311_assignment3/src/processor/pipeline/OperandFetch.java
@@ -6,6 +6,11 @@ public class OperandFetch {
 	Processor containingProcessor;
 	IF_OF_LatchType IF_OF_Latch;
 	OF_EX_LatchType OF_EX_Latch;
+
+	int bitExtracted(int number, int k, int p)
+	{
+		return (((1 << k) - 1) & (number >> (p - 1)));
+	}
 	
 	public OperandFetch(Processor containingProcessor, IF_OF_LatchType iF_OF_Latch, OF_EX_LatchType oF_EX_Latch)
 	{
@@ -19,7 +24,56 @@ public class OperandFetch {
 		if(IF_OF_Latch.isOF_enable())
 		{
 			//TODO
+			int instruction = IF_OF_Latch.getInstruction();
+			int opcode = bitExtracted(instruction, 1, 5);
+			int immediate = bitExtracted(instruction, 16, 18);
+			int branchOffset = bitExtracted(instruction, 6, 27);
+			branchOffset = branchOffset * 4;
+
+			int rs1 = 0, rs2 = 0, rd = 0;
+			if(opcode <= 21 && opcode % 2 == 1)
+			{
+				rs1 = bitExtracted(instruction, 6, 5);
+				rd = bitExtracted(instruction, 11, 5);
+				immediate = bitExtracted(instruction, 16, 17);
+			}
+			else if(opcode <= 21 && opcode % 2 == 0)
+			{
+				rs1 = bitExtracted(instruction, 6, 5);
+				rs2 = bitExtracted(instruction, 11, 5);
+				rd = bitExtracted(instruction, 16, 5);
+			}
+			else if(opcode == 24)	//jmp
+			{
+				rd = bitExtracted(instruction, 6, 5);
+				immediate = bitExtracted(instruction, 11, 22);
+			}
+			else if(opcode == 29)	//end
+			{
+				//nothing
+			}
+			else
+			{
+				rs1 = bitExtracted(instruction, 6, 5);
+				rd = bitExtracted(instruction, 11, 5);
+				immediate = bitExtracted(instruction, 16, 17);
+			}
+			int op1 = containingProcessor.getRegisterFile().getValue(rs1);
+			int op2 = 0;
 			
+			if(opcode == 23)	//store command
+			{
+				op2 = containingProcessor.getRegisterFile().getValue(rd);
+			}
+			else
+			{
+				op2 = containingProcessor.getRegisterFile().getValue(rs2);
+			}
+			
+			OF_EX_Latch.setBranchTarget(branchOffset);
+			OF_EX_Latch.setOp1(op1);
+			OF_EX_Latch.setOp1(op1);
+			OF_EX_Latch.setImm(immediate);
 			IF_OF_Latch.setOF_enable(false);
 			OF_EX_Latch.setEX_enable(true);
 		}
